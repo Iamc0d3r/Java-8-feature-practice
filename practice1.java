abc4a7306f11101c2b4449f8610d7a8ed3afe59f
unSEEN <42385110+Iamc0d3r@users.noreply.github.com>
2023-10-14 21:20:22 +0530

    practice1.java

---
+++ practice1.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class practice1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        String name = sc.next();
+        System.out.println("Hello, " + name + "!");
+    }
+}
